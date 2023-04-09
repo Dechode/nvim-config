477698a02170cf3d008dbaaad9f2df449a4b71a8
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-09 18:50:25 +0300

    Add telescope

--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -14,6 +14,11 @@ return require('packer').startup(function()
 	use 'tikhomirov/vim-glsl'
 	use 'ziglang/zig.vim'
 	-- File manager
+	use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	-- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+	}
 	use {'nvim-tree/nvim-tree.lua',
   	requires = {'nvim-tree/nvim-web-devicons', },tag = 'nightly'}
 	-- Better syntax highlighting
