feb2c6baec98c24a733c625fb0ef1c2884bc5ca3
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-12 12:20:11 +0300

    Refactor

--- init.lua
+++ init.lua
@@ -1,18 +1,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('packer-plugins')
-local lspconfig = require('lspconfig')
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'gdscript' }
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {}
-end
-
---require('nvim-cmp')
---require('treesitter')
-require("nvim-tree").setup()
-
 vim.cmd('colorscheme gruvbox')
 vim.g.mapleader = " "
 vim.o.relativenumber = true
@@ -24,5 +12,21 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
 
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 6
+
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+
+require('packer-plugins')
+local lspconfig = require('lspconfig')
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'gdscript' }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {}
+end
+
+require("nvim-tree").setup()
+
 
--- lua/packer-plugins.lua
+++ lua/packer-plugins.lua
@@ -1,32 +1,32 @@
 return require('packer').startup(function()
 	-- Plugin manager
 	use 'wbthomason/packer.nvim'
-	
+
 	-- Languages
 	use 'tikhomirov/vim-glsl'
 	use 'ziglang/zig.vim'
-	
+
 	-- File manager
 	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 	use {'nvim-tree/nvim-tree.lua',
-	  	requires = {'nvim-tree/nvim-web-devicons', },tag = 'nightly'
+		requires = {'nvim-tree/nvim-web-devicons', },tag = 'nightly'
 	}
-	
+
 	-- Startup screen
 	use {
-	  "startup-nvim/startup.nvim",
-	  requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-	  config = function()
-		require"startup".setup({theme = "dashboard"})
-	  end
+		"startup-nvim/startup.nvim",
+		requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+		config = function()
+			require"startup".setup({theme = "dashboard"})
+		end
 	}
 
 	-- Theme
 	use 'gruvbox-community/gruvbox'	
-	
+
 	-- LSP and autocompletion 
 	use 'neovim/nvim-lspconfig'
 	use 'hrsh7th/nvim-cmp'
@@ -34,10 +34,10 @@ return require('packer').startup(function()
 	use 'hrsh7th/cmp-buffer'
 	use 'hrsh7th/cmp-vsnip'
 	use 'hrsh7th/vim-vsnip'
-	
+
 	-- Better syntax highlighting
 	use ({'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"})
-	
+
 	-- Git 
 	use 'tpope/vim-fugitive'
 end)
